f32c535a69a54540e3af58a8b0338f053387cb10
Lucia Salinas Ballesteros <lusalinas@live.com.ar>
2025-10-16 21:26:54 +0200

    navbar + css base

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+    return (
+      <>
+        <nav>
+          <ul className="navBar">
+            <li>
+              <a href="home.html">Home</a>
+            </li>
+            <li>
+              <a href="contacto.html">Contacto</a>
+            </li>
+          </ul>
+        </nav>
+      </>
+    );
+}
+export default Navbar;
