35554e3a8ea38a926dcb95b85fee99ebec46c332
Lucia Salinas Ballesteros <lusalinas@live.com.ar>
2025-10-17 13:53:30 +0200

    cart widget

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,9 @@
+function Cart() {
+  return (
+    <span>
+             <img src="src/components/img/cart.png" alt="cart" />
+          </span>
+  );
+}
+
+export default Cart;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,27 @@
+import Cart from "./CartWidget"
 function Navbar() {
     return (
       <>
         <nav>
           <ul className="navBar">
             <li>
-              <a href="home.html">Home</a>
+              <a href="index.html">Home</a>
             </li>
             <li>
-              <a href="contacto.html">Contacto</a>
+              <a href="products.html">Productos y servicios</a>
             </li>
+            <li>
+              <a href="aboutUs.html">Sobre nosotros</a>
+            </li>
+            <li>
+              <a href="contact.html">Contacto</a>
+            </li>
+          <li>
+            <Cart />
+          </li>
           </ul>
         </nav>
+      
       </>
     );
 }
